31b10056d0013c3de406d0e235657805322b2458
Samira-Git <siam27most@gmail.com>
2021-02-26 15:31:38 +0100

    ajout redirection en js

--- annuler_reservation.php
+++ annuler_reservation.php
@@ -1,21 +1,26 @@
 <?php
 
-    if (isset($_POST['annuler'])) {
-        if(isset($_POST['idAnnonce'])) {
-           $id_annonce = $_POST['idAnnonce'];
-        }
-       include_once "connexion_bdd.php";
-        $requete_message = "UPDATE advert SET reservation_message=:mess WHERE id=:id";
-        $requete_message_prep = $bdd->prepare($requete_message);
-        $requete_message_prep->bindValue(":mess", null, PDO::PARAM_STR);
-        $requete_message_prep->bindValue(":id", $id_annonce, PDO::PARAM_INT);
-        if ($requete_message_prep->execute()) {
-            echo "Réservation annulée";
-            header('Refresh: 0.8; url=home.php');
-        } else {
-            $erreur = $bdd->errorInfo();
-            echo "Erreur d'envoie : " . $erreur[0] . " - Code : " . $bdd->errorCode();
-        }
-
-        $bdd =null;
+if (isset($_POST['annuler'])) {
+    if (isset($_POST['idAnnonce'])) {
+        $id_annonce = $_POST['idAnnonce'];
     }
+    include_once "connexion_bdd.php";
+    $requete_message = "UPDATE advert SET reservation_message=:mess WHERE id=:id";
+    $requete_message_prep = $bdd->prepare($requete_message);
+    $requete_message_prep->bindValue(":mess", null, PDO::PARAM_STR);
+    $requete_message_prep->bindValue(":id", $id_annonce, PDO::PARAM_INT);
+    if ($requete_message_prep->execute()) {
+        echo "Réservation annulée";
+?>
+        <SCRIPT LANGUAGE="JavaScript">
+            setTimeout("document.location.href = 'home.php'", 2000);
+        </SCRIPT>
+<?php
+        //header('Refresh: 0.8; url=home.php');
+    } else {
+        $erreur = $bdd->errorInfo();
+        echo "Erreur d'envoie : " . $erreur[0] . " - Code : " . $bdd->errorCode();
+    }
+
+    $bdd = null;
+}
--- reserver.php
+++ reserver.php
@@ -1,21 +1,26 @@
 <?php
 
-    if (isset($_POST['reservation']) && isset($_POST['message'])) {
-        $message = $_POST['message'];
-        if(isset($_POST['idAnnonce'])) {
-           // echo $_POST['idAnnonce'];
-           $id_annonce = $_POST['idAnnonce'];
-        }
-       include_once "connexion_bdd.php";
-        $requete_message = "UPDATE advert SET reservation_message=:mess WHERE id=:id";
-        $requete_message_prep = $bdd->prepare($requete_message);
-        $requete_message_prep->bindValue(":mess", $message, PDO::PARAM_STR);
-        $requete_message_prep->bindValue(":id", $id_annonce, PDO::PARAM_INT);
-        if ($requete_message_prep->execute()) {
-            echo "Votre message a été envoyé. Annonce réservée";
-            header('Refresh: 0.8; url=home.php');
-        } else {
-            $erreur = $bdd->errorInfo();
-            echo "Erreur d'envoie : " . $erreur[0] . " - Code : " . $bdd->errorCode();
-        }
+if (isset($_POST['reservation']) && isset($_POST['message'])) {
+    $message = $_POST['message'];
+    if (isset($_POST['idAnnonce'])) {
+        // echo $_POST['idAnnonce'];
+        $id_annonce = $_POST['idAnnonce'];
     }
+    include_once "connexion_bdd.php";
+    $requete_message = "UPDATE advert SET reservation_message=:mess WHERE id=:id";
+    $requete_message_prep = $bdd->prepare($requete_message);
+    $requete_message_prep->bindValue(":mess", $message, PDO::PARAM_STR);
+    $requete_message_prep->bindValue(":id", $id_annonce, PDO::PARAM_INT);
+    if ($requete_message_prep->execute()) {
+        echo "Votre message a été envoyé. Annonce réservée";
+?>
+        <SCRIPT LANGUAGE="JavaScript">
+            setTimeout("document.location.href = 'home.php'", 2000);
+        </SCRIPT>
+<?php
+       // header('Refresh: 0.8; url=home.php');
+    } else {
+        $erreur = $bdd->errorInfo();
+        echo "Erreur d'envoie : " . $erreur[0] . " - Code : " . $bdd->errorCode();
+    }
+}
